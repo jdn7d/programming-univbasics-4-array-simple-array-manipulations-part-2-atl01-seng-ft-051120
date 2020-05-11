87ead0811f05a3058f573e72d7fcd24421a90e5a
Jessie Donovan <donovanajessie@gmail.com>
2020-05-11 02:57:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array[4].insert(element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+
+end
+
+def using_delet(array, string)
+  array.delete(string)
+end 
+
